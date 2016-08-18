ff9559569735416030660acfdd67f1947dd2e581
abhijit kalta <abhijitkalta@gmail.com>
2016-08-19 01:51:08 +0530

    index page modified

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module RecipeBoxApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #for bower
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
